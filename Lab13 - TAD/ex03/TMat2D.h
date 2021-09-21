610f9589497d79a11e73bd6c64c8567a4cd8076a
alexia-martins <80076615+alexia-martins@users.noreply.github.com>
2021-09-21 06:08:10 -0300

    att

--- Lab13 - TAD/ex03/TMat2D.h
+++ Lab13 - TAD/ex03/TMat2D.h
@@ -6,4 +6,4 @@ int mat2D_free(TMat2D *mat);
 int mat2D_set_value(TMat2D *mat, int nrows, int ncolumns, double value);
 int mat2D_get_value(TMat2D *mat, int nrows, int ncolumns, double *index);
 void mat2D_acess_row_column (TMat2D *mat, int nrow, int ncolumn);
-//void mat2D_print_everything (TMat2D *mat);
+void mat2D_print_everything (TMat2D *mat);
