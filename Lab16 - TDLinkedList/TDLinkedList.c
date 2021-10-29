42f10f1bc25d38e91681c66950e543a319102ad2
alexia-martins <80076615+alexia-martins@users.noreply.github.com>
2021-10-29 06:28:47 -0300

    att

--- Lab16 - TDLinkedList/TDLinkedList.c
+++ Lab16 - TDLinkedList/TDLinkedList.c
@@ -1,5 +1,33 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include "TDLinkedList.h"
+#ifndef TDLinkedListh
+#define TDLinkedListh
+
+#define SUCCESS 0
+#define INVALID_NULL_POINTER -1
+#define OUT_OF_MEMORY -2
+#define OUT_OF_RANGE -3
+#define ELEM_NOT_FOUND -4
+
+#include "../aluno.h"
+
+typedef struct TDLinkedList TDLinkedList;
 
 
+TDLinkedList* list_create();
+int list_free(TDLinkedList* li);
+int list_push_front(TDLinkedList *li, struct student al);
+int list_push_back(TDLinkedList *li, struct student al);
+int list_insert(TDLinkedList *li, int pos, struct student sl);
+int list_size(TDLinkedList *li);
+int list_pop_front(TDLinkedList *li);
+int list_pop_back(TDLinkedList *li);
+int list_erase(TDLinkedList *li, int pos);
+int list_erase_mat(TDLinkedList *li, int id);
+int list_find_mat(TDLinkedList *li, int id, struct student *sl);
+int list_find_pos(TDLinkedList *li, int pos, struct student *sl);
+int list_front(TDLinkedList *li, struct student *sl); 
+int list_back(TDLinkedList *li, struct student *sl); 
+int list_get_pos(TDLinkedList *li, int id, int *pos); 
+int list_print_foward(TDLinkedList *li);
+int list_print_reverse(TDLinkedList *li);
+
+#endif
