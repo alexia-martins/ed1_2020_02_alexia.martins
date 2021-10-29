323d6d68610de53d154b350031f2125eea161e53
alexia-martins <80076615+alexia-martins@users.noreply.github.com>
2021-10-29 06:50:48 -0300

    att

---
+++ Lab18 - TQueue/ex03/TQueue.h
@@ -0,0 +1,27 @@
+#ifndef TQueueh
+#define TQueueh
+
+#define SUCCESS 0
+#define INVALID_NULL_POINTER -1
+#define OUT_OF_MEMORY -2
+#define OUT_OF_RANGE -3
+#define FULL -4
+#define EMPTY -5
+#define NOT_EMPTY -6
+
+#include "../aluno.h"
+
+typedef struct TQueue TQueue;
+
+TQueue* create_queue(int TAM);
+int free_queue(TQueue *fi);
+int enqueue(TQueue *fi, struct student sl);
+int dequeue(TQueue *fi);
+int queue_front(TQueue *fi, struct student *sl);
+int queue_empty(TQueue *fi);
+int print_queue(TQueue *fi); //testes
+int size(TQueue *fi);
+int suport_queue(TQueue* fi);
+TQueue* compact_queue(TQueue *fi);
+
+#endif
